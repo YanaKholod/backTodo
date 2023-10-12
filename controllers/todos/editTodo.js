ca7430cddbb74b85e220819ec353fe70028c303a
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-10-12 15:07:12 +0300

    upd

--- controllers/todos/editTodo.js
+++ controllers/todos/editTodo.js
@@ -4,36 +4,31 @@ const Todo = require("../../models/Todo");
 const editTodo = async (req, res) => {
   const { id } = req.params;
   const updateInfo = req.body;
-  
 
   if (updateInfo.parentTodo) {
-    // It's an update for a sub-todo
     const parentTodo = await Todo.findById(updateInfo.parentTodo);
 
     if (!parentTodo) {
       throw new HttpError(404, "Parent Todo not found");
     }
 
-    // Push the new sub-todo into the parent's subTodos array
     parentTodo.subTodos.push(updateInfo);
 
     const result = await parentTodo.save();
-    
- if (!result) {
+
+    if (!result) {
       throw new HttpError(500, "Failed to save sub-todo");
     }
-
     res.json("Sub-Todo added successfully");
   } else {
-    // It's an update for the parent todo
     const result = await Todo.findByIdAndUpdate(id, updateInfo, {
       new: true,
     });
-    
-  if (!result) {
-    throw new HttpError(404, "Not found");
-  }
-  res.json("Update success");
-};
 
+    if (!result) {
+      throw new HttpError(404, "Not found");
+    }
+    res.json("Update success");
+  }
+};
 module.exports = editTodo;
