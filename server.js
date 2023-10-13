21008e62cc6d56be36b03601c610f0d20761d990
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-10-13 16:44:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ mongoose.connect(DB_HOST).then(() => {
   app
     .listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+    });
     })
     .catch((error) => {
       console.error("MongoDB connection error:", error);
       process.exit(1); // выходит из процесса и завершает работу нод джс и код (1) значит выход с ошибкой
     });
-});
+
