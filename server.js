9f5e8a047ef48c66151a08e22ebf582ad8a4d0c7
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-10-13 16:41:35 +0300

    upd

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const app = require("./index");
 const mongoose = require("mongoose");
-const WebSocket = require("ws");
 
 const { DB_HOST } = process.env;
 const PORT = 8080;
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    const server = app.listen(PORT, () => {
+mongoose.connect(DB_HOST).then(() => {
+  app
+    .listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+      process.exit(1); // выходит из процесса и завершает работу нод джс и код (1) значит выход с ошибкой
     });
-   
-
-  .catch((error) => {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // выходит из процесса и завершает работу нод джс и код (1) значит выход с ошибкой
-  });
+});
