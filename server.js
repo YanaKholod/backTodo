f6dca59b093324593c18bb4f44144ea68adc6978
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-10-13 16:33:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,25 +11,7 @@ mongoose
     const server = app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
-    const wss = new WebSocket.Server({ server }); // создание подключения
-
-    // подтверждение что клиент подключился
-    wss.on("connection", (ws) => {
-      console.log("WebSocket client connected");
-
-      // отлов сообщений от клиента
-      ws.on("message", (message) => {
-        // Add your chatbot logic here
-        // You can process the incoming message and send a response back to the client
-        ws.send(`Received: ${message}`);
-      });
-
-      // закрытие соединения
-      ws.on("close", () => {
-        console.log("WebSocket client disconnected");
-      });
-    });
-  })
+   
 
   .catch((error) => {
     console.error("MongoDB connection error:", error);
