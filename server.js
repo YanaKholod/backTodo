52de4540cf7896d731a408fbb06696679780060c
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-10-18 14:03:49 +0300

    upd

--- server.js
+++ server.js
@@ -1,17 +1,58 @@
 const app = require("./index");
 const mongoose = require("mongoose");
+const WebSocket = require("ws");
 
 const { DB_HOST } = process.env;
 const PORT = 8080;
 
-mongoose.connect(DB_HOST).then(() => {
-  app
-    .listen(PORT, () => {
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    const server = app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
-    })
-    .catch((error) => {
-      console.error("MongoDB connection error:", error);
-      process.exit(1); // выходит из процесса и завершает работу нод джс и код (1) значит выход с ошибкой
+
+    const wss = new WebSocket.Server({ server });
+
+    wss.on("connection", (ws) => {
+      console.log("A client connected to the WebSocket");
+
+      ws.on("message", (message) => {
+        wss.clients.forEach((client) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message);
+          }
+        });
+      });
+
+      ws.on("close", () => {
+        console.log("A client disconnected from the WebSocket");
+      });
     });
 
+    wss.on("error", (error) => {
+      console.error("WebSocket error:", error);
+    });
+    process.on("SIGINT", () => {
+      server.close(() => {
+        console.log("Server and WebSocket closed.");
+        process.exit(0);
+      });
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("MongoDB connection error:", error);
+//     process.exit(1); // выходит из процесса и завершает работу нод джс и код (1) значит выход с ошибкой
+//   });
